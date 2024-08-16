b16797bd7102d5fa0bf2f5b909e7aa085e3e306b
shahidullah177 <mdshahidsumon177@gmail.com>
2024-08-17 01:16:18 +0600

    Pagination function

--- index.js
+++ index.js
@@ -14,33 +14,44 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qcso25z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-   const productCollection = client.db('productsDB').collection('products'); 
+    try {
+        const productCollection = client.db('productsDB').collection('products');
 
-    app.get('/products', async(req, res) => {
-        const data = productCollection.find();
-        const productData = await data.toArray();
-        res.send(productData);
-    })
-   
-    app.get('/products/:id', async(req, res) => {
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id)}
-        const productData = await productCollection.findOne(query);
-        res.send(productData);
-    })
+        app.get('/products', async (req, res) => {
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
 
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-  }
+            const productData = await productCollection
+                .find()
+                .skip(page * size)
+                .limit(size)
+                .toArray();
+            res.send(productData);
+        })
+
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const productData = await productCollection.findOne(query);
+            res.send(productData);
+        })
+
+        app.get('/productsCount', async (req, res) => {
+            const count = await productCollection.estimatedDocumentCount();
+            res.send({ count });
+        })
+
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+    }
 }
 run().catch(console.dir);
 
